d912cedd0ff58674f1df0b93921eae0bf5b7b58a
simon2828 <simon.tanner28@gmail.com>
2018-10-22 11:02:47 +0100

    update next

---
+++ pages/step-to-success.js
@@ -0,0 +1,42 @@
+import Head from 'next/head'
+import Layout from '../components/Layout.js'
+import MobileMenu from '../components/MobileMenu.js'
+import Header from '../components/Header.js'
+import NoSSR from 'react-no-ssr';
+import LoadingIndicator from '../components/LoadingIndicator.js'
+import SpeechToText from '../components/SpeechToText'
+
+
+
+class Index extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            mobileMenu: true, 
+            recording: false
+        }
+        this.toggleMenu = this.toggleMenu.bind(this);
+    }
+
+
+    toggleMenu() {
+        this.setState({ mobileMenu: !this.state.mobileMenu })
+    }
+
+    render() {
+        let mobileMenu = this.state.mobileMenu ? '-hidden' : ''
+        return (
+            <div>
+                <Header toggleMenu={this.toggleMenu} />
+                <Layout>
+                    <MobileMenu hidden={mobileMenu} toggleMenu={this.toggleMenu} />
+                    <NoSSR onSSR={<LoadingIndicator />}>  
+                        <SpeechToText />                   
+                    </NoSSR>
+                </Layout>
+            </div>
+        )
+    }
+}
+
+export default Index
---
+++ styles.js
@@ -0,0 +1,5 @@
+import css from 'styled-jsx/css';
+
+export const colors = css`
+    primary: yellow;
+    `
